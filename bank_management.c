87f8f4622a52cd789948832fed805a3c5f91af6c
yiannis <107484245+DimYiannis@users.noreply.github.com>
2024-03-13 20:21:48 +0200

    create account and login functions
    using struct

--- bank_management.c
+++ bank_management.c
@@ -1,21 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct Account {
+    char name[50];
+    char pass[50];
+    int isLoggedIn;
+};
 
-void createacc() {
+void createacc(struct Account *accounts) {
 
     char name[50];
     char pass[50];
 
-    printf("Please insert your name");
+    printf("Please insert your name: \n");
+    scanf("%s", &name);
+    printf("\n Please create your password: \n");
+    scanf("%s", &pass);
+    printf("\n Your new account has been created!");
+    printf("\nProceed to login... \n");
 
 }
 
+void login(struct Account *accounts, int numAccounts) {
+  char name[50];
+  char pass[50];
+  int i;
 
+  printf("Enter your name: ");
+  scanf("%s", name);
+  printf("%s", pass);
+
+    for (i=0; i<numAccounts; i++)
+    {
+        if (strcpm(accounts[i].name, name)== 0 &&
+         strcpm(accounts[i].pass, pass) == 0)
+        {
+            accounts[i].isLoggedIn = 1;
+            printf("\nWelcome, %s! You are now logged in.\n", name);
+            return;
+        }   
+    }
+    // If no matching account found
+    printf("\nInvalid username or password. Please try again.\n");
+}
 
 int main()
 {
     int choice;
+    struct Account accounts[100];
 
     while (1)
     {
@@ -27,11 +59,12 @@ int main()
 
         if (choice == 1)
         {
-            createacc();
+            createacc(accounts);
+            login(accounts, 100);
         }
         else if (choice == 2)
         {
-            login();
+            login(accounts, 100);
         }
         else
         {
